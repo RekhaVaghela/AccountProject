8e689398e097e3b89a3c2cd7ece1ebca26e6d395
ak623 <ashoklimbu@live.co.uk>
2018-08-24 10:29:12 +0100

    first commit

--- src/test/java/com/qa/quickstart/accountProject/AppTest.java
+++ src/test/java/com/qa/quickstart/accountProject/AppTest.java
@@ -4,35 +4,15 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest extends TestCase{
+	public void test(){
+		int actual_value;
+		serviceClass service = new serviceClass();
+		service.addaccountClasstoMap(new accountClass("Rekha" , "Vaghela" , 1234));
+		service.addaccountClasstoMap(new accountClass("Jess" , "Williams" , 1122));
+		service.addaccountClasstoMap(new accountClass("Jess" , "Williams" , 1123));
+		actual_value = service.getTotalfirstName();
+		assertEquals(2,actual_value);
+	}
+	
 }
